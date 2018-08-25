9356a6d11649e4da4dfec8ea5bc86d18b0c7d254
Peter Barclay <peter@peter-barclay.com>
2018-08-25 10:52:30 +0100

    add SampleApp-iOS project

---
+++ SampleApp-iOS/SampleApp-iOSTests/SampleApp_iOSTests.swift
@@ -0,0 +1,15 @@
+//
+//  Copyright © 2018 Peter Barclay. All rights reserved.
+//
+
+import XCTest
+@testable import SampleApp_iOS
+
+class SampleApp_iOSTests: XCTestCase {
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
